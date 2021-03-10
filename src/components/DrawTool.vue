84b8043ff9ec144c4f9c49d389432afa886970d3
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-03-10 09:48:57 +0900

    fix 自動フォーマットを適用

--- src/components/DrawTool.vue
+++ src/components/DrawTool.vue
@@ -2,7 +2,12 @@
   <div>
     <h1>画像を表示</h1>
     <div id="canvas-area">
-      <canvas id="myCanvas" width="250px" height="250px" class="canvas"></canvas>
+      <canvas
+        id="myCanvas"
+        width="250px"
+        height="250px"
+        class="canvas"
+      ></canvas>
     </div>
   </div>
 </template>
@@ -16,14 +21,14 @@
 <script>
 export default {
   name: "DrawTool",
-  props: ['uploadedImage',"canvasText","fontVar","colors"],
+  props: ["uploadedImage", "canvasText", "fontVar", "colors"],
   data() {
     return {
       canvas: null,
       context: null,
       isDrag: false,
       max_width: 250,
-      max_height: 250
+      max_height: 250,
     };
   },
 
@@ -34,43 +39,42 @@ export default {
     this.context.lineJoin = "round";
     this.context.lineWidth = 5;
     this.context.strokeStyle = "#000000";
-    this.drawImage(this.canvas)
+    this.drawImage(this.canvas);
   },
   methods: {
     //キャンバスに画像を描画
-      drawImage: async function(canvas) {
-        const image = await this.asyncLoadImage()
-        const self = this
-        if(image.width > self.max_width){
-          image.width = self.max_width;
-        }
-        if(image.height > self.max_height){
-          image.height = self.max_height;
-        }
-        const ctx = canvas.getContext("2d")
+    drawImage: async function (canvas) {
+      const image = await this.asyncLoadImage();
+      const self = this;
+      if (image.width > self.max_width) {
+        image.width = self.max_width;
+      }
+      if (image.height > self.max_height) {
+        image.height = self.max_height;
+      }
+      const ctx = canvas.getContext("2d");
 
-        // キャンバスに画像を描画（開始位置0,0）
-        ctx.drawImage(image, 0, 0,image.width,image.height);
-        console.log(image.width);
-        console.log(image.height);
-      },
-    
+      // キャンバスに画像を描画（開始位置0,0）
+      ctx.drawImage(image, 0, 0, image.width, image.height);
+      console.log(image.width);
+      console.log(image.height);
+    },
 
-    drawText: function(canvas, text) {
+    drawText: function (canvas, text) {
       const ctx = canvas.getContext("2d");
       //TODO: はじめに既存文字を削除
-      
+
       //文字のスタイルを指定
-      if(this.fontVar == ""){
+      if (this.fontVar == "") {
         ctx.font = "32px San Francisco"; //ここを変数に
-      }else{
-        ctx.font = `32px ${this.fontVar}`
+      } else {
+        ctx.font = `32px ${this.fontVar}`;
       }
       // ctx.font = fontVar;
 
-      if(colors == ""){
+      if (colors == "") {
         ctx.fillStyle = "#404040";
-      }else{
+      } else {
         ctx.fillStyle = colors;
       }
       //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
@@ -80,47 +84,47 @@ export default {
       var x = canvas.width / 2;
       var y = canvas.height / 2;
 
-        ctx.fillText(text, x, y);
-        console.log(text);  
+      ctx.fillText(text, x, y);
+      console.log(text);
     },
 
-    asyncLoadImage: async function() {
+    asyncLoadImage: async function () {
       return new Promise((resolve, reject) => {
         const img = new Image();
         img.onload = () => resolve(img);
         img.onerror = (e) => reject(e);
-        if(! this.uploadedImage) {
-          img.src = "/img/img.jpg"
-        }else{
+        if (!this.uploadedImage) {
+          img.src = "/img/img.jpg";
+        } else {
           img.src = this.uploadedImage;
         }
       });
     },
   },
 
-  watch:{
-    uploadedImage: function(){
-      this.drawImage(this.canvas)
+  watch: {
+    uploadedImage: function () {
+      this.drawImage(this.canvas);
     },
-    canvasText: function(){
-      this.context.clearRect(0, 0, 250, 250)
-      this.drawImage(this.canvas).then(()=>{
-        this.drawText(this.canvas, this.canvasText)
-      })
+    canvasText: function () {
+      this.context.clearRect(0, 0, 250, 250);
+      this.drawImage(this.canvas).then(() => {
+        this.drawText(this.canvas, this.canvasText);
+      });
     },
 
-    fontVar: function(){
-      this.context.clearRect(0, 0, 250, 250)
-      this.drawImage(this.canvas).then(()=>{
-        this.drawText(this.canvas, this.canvasText)
-      })
+    fontVar: function () {
+      this.context.clearRect(0, 0, 250, 250);
+      this.drawImage(this.canvas).then(() => {
+        this.drawText(this.canvas, this.canvasText);
+      });
     },
-    colors: function() {
-      this.context.clearRect(0, 0, 250, 250)
-      this.drawImage(this.canvas).then(()=>{
-      this.drawText(this.canvas, this.canvasText)
-      })
-    }
+    colors: function () {
+      this.context.clearRect(0, 0, 250, 250);
+      this.drawImage(this.canvas).then(() => {
+        this.drawText(this.canvas, this.canvasText);
+      });
+    },
   },
 };
 </script>
